84a7fd8d378d989528767a866046e19db7715dc7
Andrew Chisholm <andrew.chisholm@hsdc.ac.uk>
2021-12-13 09:07:35 +0000

    add timing

--- ReadJSON.py
+++ ReadJSON.py
@@ -1,4 +1,9 @@
 import requests
+import time
 url = 'http://127.0.0.1:5000/'
+start_the_clock = time.time_ns()
 re = requests.get(url)
+stop_the_clock = time.time_ns()
+
 print(re.json())
+print((stop_the_clock - start_the_clock)/1000000000)
